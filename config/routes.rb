2414677fcbfa32e2354d217aa3988be574973062
Noah Adams <nhadams10@uchicago.edu>
2021-02-23 22:24:12 +0000

    user details looks solid

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
 
-  get("/users/:user", { :controller => "users", :action =>})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
 end
